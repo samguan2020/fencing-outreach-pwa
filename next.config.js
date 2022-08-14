f4900074ffa6698f86348662b538e4baa5f497b5
Sam Guan <xin.guan@gmail.com>
2022-08-14 12:48:35 -0700

    second commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      'www.shareicon.net',
+      'lh3.googleusercontent.com'
+    ],
+  }
+}
+
+module.exports = nextConfig
