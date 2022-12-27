44984ed877359f0598bba2bd27d25c1edf410d26
Xin Guan <xin.guan@gmail.com>
2022-12-26 22:19:21 -0800

    fix bugs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NextPage } from 'next';
-import { footerList1, footerList2, footerList3 } from '../utils/constants';
+import { footerList1 } from '../utils/constants';
 
 const List = ({ items, mt }: { items: string[], mt: Boolean }) => (
   <div className={`flex flex-wrap gap-2 ${mt && 'mt-5'}`}>
