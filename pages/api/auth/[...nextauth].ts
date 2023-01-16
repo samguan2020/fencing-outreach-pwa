30a4c036c55fa3b639a3d58a54fa97130a3bd7de
Xin Guan <xin.guan@gmai.com>
2023-01-15 17:06:08 -0800

    remove github oauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
-import GithubProvider from "next-auth/providers/github"
 
 export default NextAuth({
     // Configure one or more authentication providers
@@ -8,10 +7,6 @@ export default NextAuth({
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        }),
-        GithubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
         }),
         // ...add more providers here
     ],
